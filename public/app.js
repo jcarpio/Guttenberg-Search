50b254cafbac1672ee8ac68d4cbe041a74157263
Jose Carpio <jose.carpio@gmail.com>
2019-11-22 11:43:09 +0100

    camarón

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ const vm = new Vue ({
   data () {
     return {
       baseUrl: 'http://localhost:3000', // API url
-      searchTerm: 'Flamenco', // Default search term
+      searchTerm: 'Camarón', // Default search term
       searchDebounce: null, // Timeout for search bar debounce
       searchResults: [], // Displayed search results
       numHits: null, // Total search results found
